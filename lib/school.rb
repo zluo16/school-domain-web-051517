9158a0fe9dbfb144c0e2a5443827360bb33803fa
Flatiron School <flatironschool@Flatirons-MacBook-Air.local>
2017-05-16 18:11:31 -0400

    made School class

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,39 @@
-# code here!
+class School
+
+  def add_student(name, grade)
+
+    if @rost.keys.include?(grade)
+      @rost[grade] << name
+    else
+      @rost[grade] = Array.new.push(name)
+    end
+  end
+
+  def roster
+    @rost
+  end
+
+  def initialize(name)
+    @name = name
+    @rost = {}
+    roster
+  end
+
+  def name
+    @name
+  end
+
+  def grade(num)
+    @rost[num]
+  end
+
+  def sort
+    new_hash = {}
+
+    @rost.each do |grade, array|
+      new_hash[grade] = array.sort
+    end
+    new_hash
+  end
+
+end
